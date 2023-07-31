0fbbbe9d107a82af4eaaca232225b7345ca95888
phemi <mrlapinho@gmail.com>
2023-07-31 02:16:38 +0100

    conditionally show "Add New Task" button

--- src/components/Dashboard/Board/TaskForm.tsx
+++ src/components/Dashboard/Board/TaskForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 
 import * as Form from "@radix-ui/react-form";
@@ -12,123 +14,129 @@ import TaskStatus from "./TaskStatus";
 const statusList = ["todo", "doing", "done"];
 
 const TaskForm = () => {
-  const [subtasks, setSubtasks] = useState<Subtask[]>([]);
+    const [subtasks, setSubtasks] = useState<Subtask[]>([]);
 
-  const addSubTask = () => {
-    const newSubTask = {
-      id: crypto.randomUUID(),
-      name: "",
-      isCompleted: false,
+    const addSubTask = () => {
+        const newSubTask = {
+            id: crypto.randomUUID(),
+            name: "",
+            isCompleted: false,
+        };
+        setSubtasks([...subtasks, newSubTask]);
     };
-    setSubtasks([...subtasks, newSubTask]);
-  };
 
-  const deleteSubTask = (id: string) => {
-    setSubtasks(subtasks.filter((subtask) => subtask.id !== id));
-  };
+    const deleteSubTask = (id: string) => {
+        setSubtasks(subtasks.filter((subtask) => subtask.id !== id));
+    };
 
-  const updateSubTask = (id: string, name: string) => {
-    setSubtasks(
-      subtasks.map((subtask) => {
-        if (subtask.id === id) {
-          return {
-            ...subtask,
-            name,
-          };
-        }
-        return subtask;
-      })
-    );
-  };
+    const updateSubTask = (id: string, name: string) => {
+        setSubtasks(
+            subtasks.map((subtask) => {
+                if (subtask.id === id) {
+                    return {
+                        ...subtask,
+                        name,
+                    };
+                }
+                return subtask;
+            })
+        );
+    };
 
-  return (
-    <>
-      <Form.Root className="space-y-6">
-        <Form.Field className="grid" name="title">
-          <div className="flex items-baseline justify-between mb-2">
-            <Form.Label className="text-kb-grey-dark-secondary dark:text-white heading-sm tracking-normal">
-              Title
-            </Form.Label>
-            <Form.Message className="text-xs text-white" match="valueMissing">
-              Please enter task title
-            </Form.Message>
-          </div>
-          <Form.Control asChild>
-            <input
-              className=" text-kb-black-primary dark:text-white rounded border border-kb-grey-dark-secondary/25 bg-transparent py-2 pl-4 pr-2 placeholder:copy-lg outline-none focus:border-kb-purple-primary copy-lg"
-              type="text"
-              required
-              placeholder="e.g. Take coffee break"
-            />
-          </Form.Control>
-        </Form.Field>
+    return (
+        <>
+            <Form.Root className="space-y-6">
+                <Form.Field className="grid" name="title">
+                    <div className="flex items-baseline justify-between mb-2">
+                        <Form.Label className="text-kb-grey-dark-secondary dark:text-white heading-sm tracking-normal">
+                            Title
+                        </Form.Label>
+                        <Form.Message
+                            className="text-xs text-white"
+                            match="valueMissing"
+                        >
+                            Please enter task title
+                        </Form.Message>
+                    </div>
+                    <Form.Control asChild>
+                        <input
+                            className=" text-kb-black-primary dark:text-white rounded border border-kb-grey-dark-secondary/25 bg-transparent py-2 pl-4 pr-2 placeholder:copy-lg outline-none focus:border-kb-purple-primary copy-lg"
+                            type="text"
+                            required
+                            placeholder="e.g. Take coffee break"
+                        />
+                    </Form.Control>
+                </Form.Field>
 
-        <Form.Field className="grid" name="description">
-          <div className="flex items-baseline justify-between mb-2">
-            <Form.Label className="text-kb-grey-dark-secondary dark:text-white heading-sm tracking-normal">
-              Description
-            </Form.Label>
-            <Form.Message className="text-xs text-white" match="valueMissing">
-              Please enter task description
-            </Form.Message>
-          </div>
-          <Form.Control asChild>
-            <textarea
-              className="text-kb-black-primary dark:text-white rounded border border-kb-grey-dark-secondary/25 bg-transparent py-2 pl-4 pr-2 placeholder:copy-lg outline-none focus:border-kb-purple-primary copy-lg"
-              required
-              placeholder="e.g. It’s always good to take a break. This 15 minute break will 
+                <Form.Field className="grid" name="description">
+                    <div className="flex items-baseline justify-between mb-2">
+                        <Form.Label className="text-kb-grey-dark-secondary dark:text-white heading-sm tracking-normal">
+                            Description
+                        </Form.Label>
+                        <Form.Message
+                            className="text-xs text-white"
+                            match="valueMissing"
+                        >
+                            Please enter task description
+                        </Form.Message>
+                    </div>
+                    <Form.Control asChild>
+                        <textarea
+                            className="text-kb-black-primary dark:text-white rounded border border-kb-grey-dark-secondary/25 bg-transparent py-2 pl-4 pr-2 placeholder:copy-lg outline-none focus:border-kb-purple-primary copy-lg"
+                            required
+                            placeholder="e.g. It’s always good to take a break. This 15 minute break will 
               recharge the batteries a little."
-              rows={5}
-            />
-          </Form.Control>
-        </Form.Field>
+                            rows={5}
+                        />
+                    </Form.Control>
+                </Form.Field>
 
-        <div>
-          <p className="text-kb-grey-dark-secondary dark:text-white heading-sm tracking-normal mb-2">
-            Subtasks
-          </p>
+                <div>
+                    <p className="text-kb-grey-dark-secondary dark:text-white heading-sm tracking-normal mb-2">
+                        Subtasks
+                    </p>
 
-          <div className="space-y-3 mb-3">
-            {subtasks.map((subtask) => (
-              <Column
-                key={subtask.id}
-                column={{
-                  columnId: subtask.id,
-                  name: subtask.name,
-                }}
-                onDelete={deleteSubTask}
-                onUpdate={updateSubTask}
-              />
-            ))}
-          </div>
+                    <div className="space-y-3 mb-3">
+                        {subtasks.map((subtask) => (
+                            <Column
+                                key={subtask.id}
+                                column={{
+                                    columnId: subtask.id,
+                                    name: subtask.name,
+                                }}
+                                onDelete={deleteSubTask}
+                                onUpdate={updateSubTask}
+                            />
+                        ))}
+                    </div>
 
-          <Button
-            size="large"
-            intent="secondary"
-            className="w-full"
-            type="button"
-            onClick={(e) => {
-              e.preventDefault();
-              addSubTask();
-            }}
-          >
-            + Add New Subtask
-          </Button>
-        </div>
+                    <Button
+                        size="large"
+                        intent="secondary"
+                        className="w-full"
+                        type="button"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            addSubTask();
+                        }}
+                    >
+                        + Add New Subtask
+                    </Button>
+                </div>
 
-        <div>
-          <h3 className="heading-sm text-kb-grey-dark-secondary dark:text-white mb-2">
-            Status
-          </h3>
-          <TaskStatus statusList={statusList} />
-        </div>
+                <div>
+                    <h3 className="heading-sm text-kb-grey-dark-secondary dark:text-white mb-2">
+                        Status
+                    </h3>
+                    <TaskStatus statusList={statusList} />
+                </div>
 
-        <Button size="large" className="w-full">
-          Create Task
-        </Button>
-      </Form.Root>
-    </>
-  );
+                <Button size="large" className="w-full">
+                    Create Task
+                </Button>
+            </Form.Root>
+        </>
+    );
 };
 
 export default TaskForm;
--- src/components/Dashboard/Nav/Nav.tsx
+++ src/components/Dashboard/Nav/Nav.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useParams } from "next/navigation";
 
 import * as Popover from "@radix-ui/react-popover";
 import { twMerge } from "tailwind-merge";
@@ -19,122 +19,129 @@ import SideNav from "./SideNav";
 import SideNavTrigger from "./SideNavTrigger";
 
 const Nav = () => {
-  const pathname = usePathname();
+    const pathname = usePathname();
+    const { boardid, taskid } = useParams();
 
-  const isSideNavOpen = useSideNavigationStore((state) => state.isOpen);
+    const isSideNavOpen = useSideNavigationStore((state) => state.isOpen);
 
-  useEffect(() => {
-    const dashboardContent = document.getElementById("dashboard-content");
-    if (dashboardContent) {
-      dashboardContent.dataset.sideNavOpen = String(isSideNavOpen);
-    }
-  }, [isSideNavOpen, pathname]);
+    useEffect(() => {
+        const dashboardContent = document.getElementById("dashboard-content");
+        if (dashboardContent) {
+            dashboardContent.dataset.sideNavOpen = String(isSideNavOpen);
+        }
+    }, [isSideNavOpen, pathname]);
 
-  return (
-    <>
-      <div
-        className={twMerge(
-          "container bg-white dark:bg-kb-black-tertiary flex items-center gap-x-5 md:border-b dark:border-kb-grey-dark-primary duration-200",
-          isSideNavOpen ? "md:ml-[261px] md:w-[calc(100%-261px)]" : ""
-        )}
-      >
-        {/* Logo */}
-        <Link
-          href="/dashboard"
-          className={twMerge(
-            "pt-5 pb-[19px] md:pr-5 md:border-r dark:border-kb-grey-dark-primary",
-            isSideNavOpen ? "hidden" : ""
-          )}
-        >
-          <LogoMobile className="md:hidden" />
-          <LogoDark className="hidden dark:md:block text-white" />
-          <LogoLight className="hidden md:block dark:md:hidden text-kb-black-primary" />
-        </Link>
+    return (
+        <>
+            <div
+                className={twMerge(
+                    "container bg-white dark:bg-kb-black-tertiary flex items-center gap-x-5 md:border-b dark:border-kb-grey-dark-primary duration-200",
+                    isSideNavOpen ? "md:ml-[261px] md:w-[calc(100%-261px)]" : ""
+                )}
+            >
+                {/* Logo */}
+                <Link
+                    href="/dashboard"
+                    className={twMerge(
+                        "pt-5 pb-[19px] md:pr-5 md:border-r dark:border-kb-grey-dark-primary",
+                        isSideNavOpen ? "hidden" : ""
+                    )}
+                >
+                    <LogoMobile className="md:hidden" />
+                    <LogoDark className="hidden dark:md:block text-white" />
+                    <LogoLight className="hidden md:block dark:md:hidden text-kb-black-primary" />
+                </Link>
 
-        <MobileMenu />
+                <MobileMenu />
 
-        {/* Project name */}
-        <h1 className="heading-lg text-kb-black-primary dark:text-white hidden md:block mt-1">
-          Platform Launch
-        </h1>
+                {/* Project name */}
+                <h1 className="heading-lg text-kb-black-primary dark:text-white hidden md:block mt-1">
+                    Platform Launch
+                </h1>
 
-        {/* Add task, edit and delete board ctas */}
-        <div className="flex items-center gap-x-4 ml-auto pt-5 pb-[19px]">
-          <Link href={`${pathname}/new-task`}>
-            <Button size="small">
-              <svg
-                width="12"
-                height="12"
-                xmlns="http://www.w3.org/2000/svg"
-                className="md:hidden"
-              >
-                <path
-                  fill="#FFF"
-                  d="M7.368 12V7.344H12V4.632H7.368V0H4.656v4.632H0v2.712h4.656V12z"
-                />
-              </svg>
-              <span className="hidden md:block heading-md">+ Add New Task</span>
-            </Button>
-          </Link>
-          <BoardActions />
-        </div>
-      </div>
+                {/* Add task, edit and delete board ctas */}
+                <div className="flex items-center gap-x-4 ml-auto pt-5 pb-[19px]">
+                    {boardid && !taskid && (
+                        <Link href={`${pathname}/new-task`}>
+                            <Button size="small">
+                                <svg
+                                    width="12"
+                                    height="12"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="md:hidden"
+                                >
+                                    <path
+                                        fill="#FFF"
+                                        d="M7.368 12V7.344H12V4.632H7.368V0H4.656v4.632H0v2.712h4.656V12z"
+                                    />
+                                </svg>
+                                <span className="hidden md:block heading-md">
+                                    + Add New Task
+                                </span>
+                            </Button>
+                        </Link>
+                    )}
+                    <BoardActions />
+                </div>
+            </div>
 
-      <SideNav />
-      <SideNavTrigger />
-    </>
-  );
+            <SideNav />
+            <SideNavTrigger />
+        </>
+    );
 };
 
 export default Nav;
 
 // Edit and delete board popover
 const BoardActions = () => {
-  const { open } = useDeleteModalStore();
+    const { open } = useDeleteModalStore();
 
-  const pathname = usePathname();
+    const pathname = usePathname();
 
-  return (
-    <Popover.Root>
-      <Popover.Trigger asChild className="cursor-pointer">
-        <svg width="5" height="20" xmlns="http://www.w3.org/2000/svg">
-          <g fill="#828FA3" fillRule="evenodd">
-            <circle cx="2.308" cy="2.308" r="2.308" />
-            <circle cx="2.308" cy="10" r="2.308" />
-            <circle cx="2.308" cy="17.692" r="2.308" />
-          </g>
-        </svg>
-      </Popover.Trigger>
+    return (
+        <Popover.Root>
+            <Popover.Trigger asChild className="cursor-pointer">
+                <svg width="5" height="20" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="#828FA3" fillRule="evenodd">
+                        <circle cx="2.308" cy="2.308" r="2.308" />
+                        <circle cx="2.308" cy="10" r="2.308" />
+                        <circle cx="2.308" cy="17.692" r="2.308" />
+                    </g>
+                </svg>
+            </Popover.Trigger>
 
-      <Popover.Portal>
-        <Popover.Content
-          className="space-y-4 bg-white dark:bg-kb-black-secondary p-4 outline-none rounded-lg mr-4 animate-in fade-in duration-300"
-          sideOffset={20}
-        >
-          <Popover.Close
-            asChild
-            className="PopoverClose cursor-pointer"
-            aria-label="Close"
-          >
-            <Link
-              href={`${pathname}/edit`}
-              className="text-kb-grey-dark-secondary copy-lg"
-            >
-              Edit Board
-            </Link>
-          </Popover.Close>
-          <Popover.Close
-            asChild
-            className="PopoverClose cursor-pointer"
-            aria-label="Close"
-            onClick={() => {
-              open("board");
-            }}
-          >
-            <p className=" text-kb-mandy-primary copy-lg">Delete Board</p>
-          </Popover.Close>
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
-  );
+            <Popover.Portal>
+                <Popover.Content
+                    className="space-y-4 bg-white dark:bg-kb-black-secondary p-4 outline-none rounded-lg mr-4 animate-in fade-in duration-300"
+                    sideOffset={20}
+                >
+                    <Popover.Close
+                        asChild
+                        className="PopoverClose cursor-pointer"
+                        aria-label="Close"
+                    >
+                        <Link
+                            href={`${pathname}/edit`}
+                            className="text-kb-grey-dark-secondary copy-lg"
+                        >
+                            Edit Board
+                        </Link>
+                    </Popover.Close>
+                    <Popover.Close
+                        asChild
+                        className="PopoverClose cursor-pointer"
+                        aria-label="Close"
+                        onClick={() => {
+                            open("board");
+                        }}
+                    >
+                        <p className=" text-kb-mandy-primary copy-lg">
+                            Delete Board
+                        </p>
+                    </Popover.Close>
+                </Popover.Content>
+            </Popover.Portal>
+        </Popover.Root>
+    );
 };
